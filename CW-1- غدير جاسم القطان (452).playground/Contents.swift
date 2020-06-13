73f7104fa50283c0e3fdce0ed14119fa57268dc9
galqattan <g_alqattan@icloud.com>
2020-06-13 19:07:53 +0300

    Homework 1

---
+++ CW-1- غدير جاسم القطان (452).playground/Contents.swift
@@ -0,0 +1,15 @@
+let name = "Ghadeer Alqattan."
+
+var hobby1 = "painting"
+
+var hobby2 = "playing the clarinet."
+
+var age = 17
+
+var height = 1.67
+
+print ("Good Afternoon, my name is", name)
+
+print ("I like", hobby1, "and", hobby2, "I am", age, "years old")
+
+print ("and", height, "m tall.")
