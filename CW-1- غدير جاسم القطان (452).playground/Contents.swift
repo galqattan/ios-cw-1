9f12d2883a24dba297cfa48f36ba05d604af26f8
galqattan <g_alqattan@icloud.com>
2020-06-15 01:51:52 +0300

    Classwork- 1 - 452

--- CW-1- غدير جاسم القطان (452).playground/Contents.swift
+++ CW-1- غدير جاسم القطان (452).playground/Contents.swift
@@ -1,3 +1,5 @@
+// part 1
+
 let name = "Ghadeer Alqattan."
 
 var hobby1 = "painting"
@@ -13,3 +15,57 @@ print ("Good Afternoon, my name is", name)
 print ("I like", hobby1, "and", hobby2, "I am", age, "years old")
 
 print ("and", height, "m tall.")
+
+// part 2
+
+let friendName = "Batool Hussain"
+
+var friendHeight = 1.69
+
+var friendAge = 17
+
+var friendHobby1 = "painting"
+
+var friendHobby2 = "gymnastics"
+
+print ("My friend is", friendName)
+
+print ("She likes", hobby1, "and", hobby2)
+
+print ("she is", friendHeight, "m tall, and is also", friendAge, "years old.")
+
+// comparing and contrasting
+
+var age_difference = age - friendAge
+
+age != friendAge
+age == friendAge
+age < friendAge
+age <= friendAge
+age > friendAge
+age >= friendAge
+
+name != friendName
+name == friendName
+
+hobby1 != friendHobby1
+hobby1 == friendHobby1
+hobby1 != friendHobby2
+hobby1 == friendHobby2
+
+hobby2 != friendHobby2
+hobby2 == friendHobby2
+hobby2 != friendHobby1
+hobby2 == friendHobby1
+
+height != friendHeight
+height == friendHeight
+height >  friendHeight
+height >= friendHeight
+height <  friendHeight
+height <= friendHeight
+
+let bestFriends = hobby1 == friendHobby1 || hobby1 == friendHobby2 || hobby2 == friendHobby1 || hobby2 == friendHobby2 || height == friendHeight || age == friendAge || name == friendName
+
+
+
